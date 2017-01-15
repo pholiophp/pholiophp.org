677bdd7edf8247dd0be3ad90840c773071916f35
chadicus <chadwickgray@gmail.com>
2017-01-14 22:20:21 -0500

    Remove hook route as it is not used

--- src/routes.php
+++ src/routes.php
@@ -6,48 +6,6 @@ $app->get('/', function ($request, $response) {
     return $this->renderer->render($response, 'index.html', ['title' => 'Pholio - The PHP Document Archive', 'is_front' => true]);
 });
 
-$app->post('/hook', function ($request, $response, $arguments) {
-    try {
-        $signature = Util::ensureNot(
-            '',
-            $request->getHeaderLine('X-Hub-Signature'),
-            __NAMESPACE__ . '\\Exception',
-            ['No signature provided', 400]
-        );
-
-        $event = Util::ensureNot(
-            '',
-            $request->getHeaderLine('X-GitHub-Event'),
-            __NAMESPACE__ . '\\Exception',
-            ['No event provided', 400]
-        );
-
-        list($algorithm, $hash) = explode('=', $signature) + [1 => null];
-        Util::ensure(
-            $hash,
-            hash_hmac($algorithm, (string)$response->getBody(), $this->githubHookSecret),
-            '\\Exception',
-            ['Invalid signature provided', 400]
-        );
-
-        $payload = json_decode((string)$response->getBody(), true);
-        Util::ensure(
-            JSON_ERROR_NONE,
-            json_last_error(),
-            '\\Exception',
-            [json_last_error_msg(), 400]
-        );
-
-        $this->queue->send(['event' => $event, 'payload' => $payload]);
-    } catch (Exception $e) {
-        return $e->formatResponse($response);
-    } catch (Throwable $e) {
-        return Exception::fromThrowable($e, 500)->formatResponse($response);
-    }
-
-    return $response->withStatus(202);
-});
-
 $app->get('/{username}/{repos}[/{version}]', function ($request, $response, $arguments) {
 
     $owner = Util\Arrays::get($arguments, 'username');
