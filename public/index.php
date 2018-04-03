20ad07f994e12c02ae8e17540b6427a0d6bdd564
chadicus <chadwickgray@gmail.com>
2018-04-02 20:14:49 -0400

    Remove unused middleware.php

--- public/index.php
+++ public/index.php
@@ -9,9 +9,6 @@ $app = new \Slim\App($settings);
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
 
-// Register middleware
-require __DIR__ . '/../src/middleware.php';
-
 // Register routes
 require __DIR__ . '/../src/routes.php';
 
